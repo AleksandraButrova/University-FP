6fe72e5377345952a66f7377b1024f4e35a16413
AleksandraButrova <aleksandra.butrova@gmail.com>
2016-10-08 13:44:00 +0300

    Change homework 11

--- FP_hw11.py
+++ FP_hw11.py
@@ -1,15 +1,21 @@
+# Извините, я настолько быа увлечена проблемой каррирования, 
+# что забыла о условиях задачи. :)
+
 def CheckDigitCurried(n):
     def CheckDigit(dig):
-        if dig % n == 0:
+        if dig % 10 == n:
             return True
         else :
-            return False
+            if (dig == 0):
+                return False
+            else :
+                return CheckDigit(dig / 10)
     return CheckDigit
 
-print(all(CheckDigitCurried(7)(x) for x in range (0, 21, 7)))   # True
-print(all(CheckDigitCurried(7)(x) for x in range (0, 21, 8)))   # False
+print(all(CheckDigitCurried(7)(x) for x in range (7, 78, 10)))  # True
+print(all(CheckDigitCurried(7)(x) for x in range (0, 9)))       # False
 print(any(CheckDigitCurried(7)(x) for x in range (1, 21, 3)))   # True
 print(any(CheckDigitCurried(7)(x) for x in range (1, 21, 7)))   # False
-print(CheckDigitCurried(7)(14))                                 # True
-print(CheckDigitCurried(7)(15))                                 # False 
+print(CheckDigitCurried(7)(71))                                 # True
+print(CheckDigitCurried(7)(11))                                 # False 
     
