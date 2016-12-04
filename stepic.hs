91ceb905b1c94d85f869b27582f54445aa823df5
AleksandraButrova <aleksandra.butrova@gmail.com>
2016-12-05 02:51:17 +0300

    Удалила лишнее.

--- stepic.hs
+++
@@ -1,5 +0,0 @@
-perms :: [a] -> [[a]]
-perms [] = [[]]
-perms [x] = [[x]]
-perms (x:xs) = concat (map (\y -> (perms' x y)) (perms xs))
-                    where perms' x y =  map (\n -> (take n y) ++ [x] ++ (drop n y)) [0..(length y)]
